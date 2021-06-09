d2c4e077850c649297dcba89315b4cde1820561e
xys@P-7920-Tower <syhuanmieay@163.com>
2021-06-09 15:11:45 +0800

    add mobilnet-v2, v3 train shell

---
+++ train_shell/train_20210415_v3.sh
@@ -0,0 +1,17 @@
+##########################################################################
+# File Name: train_20201120.sh
+# Author: xander-sun
+# mail: youngsun_007@163.com
+# Created Time: 2020年11月20日 星期五 10时13分09秒
+#########################################################################
+#!/bin/sh
+#PATH=/home/ys/bin:/usr/local/sbin:/usr/local/bin
+#export PATH
+
+train_code_path="/home/xys/tensorflow_test/train_code/sycu-tensorflow-train/research/object_detection/"
+model_dir="/home/xys/tensorflow_test/nets/sycu_ssd_mobilenet_v3_20210415"
+python3 ${train_code_path}model_main.py \
+--alsologtostderr \
+--model_dir=${model_dir} \
+--pipeline_config_path="${model_dir}/pipeline.config"
+
